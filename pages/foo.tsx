77e2cca18b31a2b77ac2075a4b8c542a8c349e12
Hendrik Liebau <mail@hendrik-liebau.de>
2024-06-03 11:09:16 +0200

    Add a pages page that imports `useParams`

---
+++ pages/foo.tsx
@@ -0,0 +1,11 @@
+import { useParams } from "next/navigation";
+
+export default function Page() {
+  const params = useParams();
+
+  return (
+    <div>
+      <div id="params">{JSON.stringify(params?.dynamic)}</div>
+    </div>
+  );
+}
